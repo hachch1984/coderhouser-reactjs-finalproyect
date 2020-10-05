1f635aaaf36e1447540e9bda43d321f55d734a07
henry chavez <hachch1984@gmail.com>
2020-10-04 22:46:31 -0500

    fix

---
+++ build/precache-manifest.94976d73af5dcd077a7a63ec99cd1448.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8d6df666bb0b79e234903c3a9be7403",
+    "url": "/coderhouser-reactjs-finalproyect/index.html"
+  },
+  {
+    "revision": "f3bcb4c6699edf8b05a9",
+    "url": "/coderhouser-reactjs-finalproyect/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "34678fb1f59d60eb8972",
+    "url": "/coderhouser-reactjs-finalproyect/static/css/main.73dbd826.chunk.css"
+  },
+  {
+    "revision": "f3bcb4c6699edf8b05a9",
+    "url": "/coderhouser-reactjs-finalproyect/static/js/2.8159b265.chunk.js"
+  },
+  {
+    "revision": "7e064966003be15ae50d6745eb9096d5",
+    "url": "/coderhouser-reactjs-finalproyect/static/js/2.8159b265.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34678fb1f59d60eb8972",
+    "url": "/coderhouser-reactjs-finalproyect/static/js/main.9b5a7c30.chunk.js"
+  },
+  {
+    "revision": "34e7517b3314dc9006f0",
+    "url": "/coderhouser-reactjs-finalproyect/static/js/runtime-main.fe1929cd.js"
+  },
+  {
+    "revision": "e07df86cef2e721115583d61d1fb68a6",
+    "url": "/coderhouser-reactjs-finalproyect/static/media/Roboto-Bold.e07df86c.ttf"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/coderhouser-reactjs-finalproyect/static/media/Roboto-Regular.11eabca2.ttf"
+  },
+  {
+    "revision": "9449b8c6c569b7b5500ca6db82e84c4f",
+    "url": "/coderhouser-reactjs-finalproyect/static/media/flagPeru.9449b8c6.png"
+  },
+  {
+    "revision": "1b3bbc746dc208db7499f8f535a05397",
+    "url": "/coderhouser-reactjs-finalproyect/static/media/gif_loading1.1b3bbc74.gif"
+  },
+  {
+    "revision": "99914ee447e7a38ac27e12a27368f776",
+    "url": "/coderhouser-reactjs-finalproyect/static/media/henry.99914ee4.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/coderhouser-reactjs-finalproyect/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fba7af5df98d3160a7934c8461d162cb",
+    "url": "/coderhouser-reactjs-finalproyect/static/media/logoCoderHouse.fba7af5d.png"
+  },
+  {
+    "revision": "a7bd62f4f6f2f05d44c2557bb28be781",
+    "url": "/coderhouser-reactjs-finalproyect/static/media/logoComplete.a7bd62f4.png"
+  },
+  {
+    "revision": "0339384d30bc4e19ad3d450c7035031f",
+    "url": "/coderhouser-reactjs-finalproyect/static/media/logoHands.0339384d.png"
+  },
+  {
+    "revision": "74ad4e07868ddc8fa6044117eb79bfcd",
+    "url": "/coderhouser-reactjs-finalproyect/static/media/logoPrincipalCompleteHorizontal.74ad4e07.png"
+  },
+  {
+    "revision": "8d9c2eee0203881f55301de4d05d5ca4",
+    "url": "/coderhouser-reactjs-finalproyect/static/media/logoReact.8d9c2eee.png"
+  },
+  {
+    "revision": "b5e9166de365f80f3a9c5513e1b6e87f",
+    "url": "/coderhouser-reactjs-finalproyect/static/media/logoText.b5e9166d.png"
+  },
+  {
+    "revision": "7ed9047b48ff1ecc3412527479fd7cb1",
+    "url": "/coderhouser-reactjs-finalproyect/static/media/noImage1.7ed9047b.png"
+  }
+]);
