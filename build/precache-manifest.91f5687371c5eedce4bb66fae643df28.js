b0699130368f8f4664e99352f71438058847288d
henry chavez <hachch1984@gmail.com>
2020-10-04 22:14:14 -0500

    production

---
+++ build/precache-manifest.91f5687371c5eedce4bb66fae643df28.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ad2eafe36ccb410b12f988cd3ee4238",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83f46dd070c705427102",
+    "url": "/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "06877b4be3fe9237639d",
+    "url": "/static/css/main.234b8e25.chunk.css"
+  },
+  {
+    "revision": "83f46dd070c705427102",
+    "url": "/static/js/2.274cf976.chunk.js"
+  },
+  {
+    "revision": "7e064966003be15ae50d6745eb9096d5",
+    "url": "/static/js/2.274cf976.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06877b4be3fe9237639d",
+    "url": "/static/js/main.9ea8463f.chunk.js"
+  },
+  {
+    "revision": "35a33c2300d2d3629163",
+    "url": "/static/js/runtime-main.4d1ac8a1.js"
+  },
+  {
+    "revision": "e07df86cef2e721115583d61d1fb68a6",
+    "url": "/static/media/Roboto-Bold.e07df86c.ttf"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/static/media/Roboto-Regular.11eabca2.ttf"
+  },
+  {
+    "revision": "9449b8c6c569b7b5500ca6db82e84c4f",
+    "url": "/static/media/flagPeru.9449b8c6.png"
+  },
+  {
+    "revision": "1b3bbc746dc208db7499f8f535a05397",
+    "url": "/static/media/gif_loading1.1b3bbc74.gif"
+  },
+  {
+    "revision": "99914ee447e7a38ac27e12a27368f776",
+    "url": "/static/media/henry.99914ee4.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fba7af5df98d3160a7934c8461d162cb",
+    "url": "/static/media/logoCoderHouse.fba7af5d.png"
+  },
+  {
+    "revision": "a7bd62f4f6f2f05d44c2557bb28be781",
+    "url": "/static/media/logoComplete.a7bd62f4.png"
+  },
+  {
+    "revision": "0339384d30bc4e19ad3d450c7035031f",
+    "url": "/static/media/logoHands.0339384d.png"
+  },
+  {
+    "revision": "74ad4e07868ddc8fa6044117eb79bfcd",
+    "url": "/static/media/logoPrincipalCompleteHorizontal.74ad4e07.png"
+  },
+  {
+    "revision": "8d9c2eee0203881f55301de4d05d5ca4",
+    "url": "/static/media/logoReact.8d9c2eee.png"
+  },
+  {
+    "revision": "b5e9166de365f80f3a9c5513e1b6e87f",
+    "url": "/static/media/logoText.b5e9166d.png"
+  },
+  {
+    "revision": "7ed9047b48ff1ecc3412527479fd7cb1",
+    "url": "/static/media/noImage1.7ed9047b.png"
+  }
+]);
